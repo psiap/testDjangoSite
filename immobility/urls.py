b68a5d20588e059eb1b6d1b405ef23bf35782a29
psiap <TVpsiap@mail.ru>
2023-01-21 21:22:25 +0300

    1.0.1

--- immobility/urls.py
+++ immobility/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', about ,name='home'),
+    path('', index ,name='home'),
+    path('showcase/', showcase ,name='showcase'),
+    path('about/', about ,name='about'),
+    path('contact/', about ,name='contact'),
 ]
--- immobility/views.py
+++ immobility/views.py
@@ -4,5 +4,16 @@ from django.views.generic import ListView
 
 
 
-def about(request): #HttpRequest
+def index(request): #HttpRequest
     return render(request, 'immobility/index.html')
+
+def showcase(request): #HttpRequest
+    return render(request, 'immobility/showcase.html')
+
+#https://xn--80aq4b.xn--80adxhks/about/
+
+def about(request): #HttpRequest
+    return render(request, 'immobility/about.html')
+
+def contact(request): #HttpRequest
+    return render(request, 'immobility/contact.html')
